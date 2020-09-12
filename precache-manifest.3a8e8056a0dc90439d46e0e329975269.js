1aa7a45379cc7daf35435deec55694f6c9e94ed3
Tom <abaotom14@gmail.com>
2020-09-12 17:11:23 +0800

    Updates

---
+++ precache-manifest.3a8e8056a0dc90439d46e0e329975269.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92b7fd13e3c06348ab36a0c7fdb2e663",
+    "url": "/*/index.html"
+  },
+  {
+    "revision": "752ed7394ca68b1e762e",
+    "url": "/*/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0babbde14e333bc689a2",
+    "url": "/*/static/js/2.8aaac6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/*/static/js/2.8aaac6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752ed7394ca68b1e762e",
+    "url": "/*/static/js/main.78c7fb45.chunk.js"
+  },
+  {
+    "revision": "fb13c61423310575b922",
+    "url": "/*/static/js/runtime-main.fe77ecc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/*/static/media/logo.5d5d9eef.svg"
+  }
+]);
